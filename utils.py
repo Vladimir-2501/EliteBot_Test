34c72aedf45dd24e540b3a2471b8d203061f831b
FrorexStudio <79785071+Vladimir-2501@users.noreply.github.com>
2021-04-25 20:40:41 +0300

    initial commit

---
+++ utils.py
@@ -0,0 +1,14 @@
+import feedparser
+
+def feed_parser():
+    NewsRssFeed = {
+        'РосРеестр': 'https://rosreestr.ru/site/rss/', 
+        'Федеральная Налоговая Служба': 'https://www.nalog.ru/rn62/rss/', 
+        'Бизнес': 'https://www.liga.net/tech/own-business/rss.xml', 
+        'Технологии': 'https://www.liga.net/tech/technology/rss.xml'
+    } #выбрнные рсс источники
+    message = dict()
+    for key in NewsRssFeed.keys():
+        current_news = feedparser.parse(NewsRssFeed[key]).entries[0]
+        message[key] = current_news.title + '\n' + current_news.link
+    return message
